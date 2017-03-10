8d6acd315437c8e32c9a5b4b1989cdc4b51ad612
druscheld <druscheld@gmail.com>
2017-03-10 16:40:48 -0300

    Bounds and constraints in the single example.

--- ifscube/examples/example_single_fit.py
+++ ifscube/examples/example_single_fit.py
@@ -12,9 +12,6 @@ lines_wl = np.array([
 # Approximate redshift of the spectrum
 z = 0.0037
 
-# Loading the spectrum
-myspec = ds.Spectrum('ngc6300_nuc.fits')
-
 # Defining the initial guess for the parameters.
 
 ncomponents = 3
@@ -29,7 +26,39 @@ p0[2::5] = 3.0                  # sigma
 p0[3::5] = 0.0                  # h3
 p0[4::5] = 0.0                  # h4
 
+# Setting bounds
+
+b = []
+for i in range(ncomponents):
+    b += [[0, 1e-12]]                                                  # flux
+    b += [[lines_wl[i] * (1. + z) - 10, lines_wl[i] * (1. + z) + 10]]  # wl
+    b += [[1.5, 9]]                                                    # sigma
+    b += [[-.2, +.2]]                                                  # h3
+    b += [[-.2, +.2]]                                                  # h4
+
+# Setting the constraints
+
+c = [
+    # Keeping the same doppler shift on all lines
+    {'type': 'eq', 'fun': lambda x: x[1]/lines_wl[0] - x[6]/lines_wl[1]},
+    {'type': 'eq', 'fun': lambda x: x[6]/lines_wl[1] - x[11]/lines_wl[2]},
+
+    # And the same goes for the sigmas
+    {'type': 'eq', 'fun': lambda x: x[2]/x[1] - x[7]/x[6]},
+    {'type': 'eq', 'fun': lambda x: x[7]/x[6] - x[12]/x[11]},
+
+    # All the same h3's and h4's
+    {'type': 'eq', 'fun': lambda x: x[3] - x[8]},
+    {'type': 'eq', 'fun': lambda x: x[8] - x[13]},
+    {'type': 'eq', 'fun': lambda x: x[4] - x[9]},
+    {'type': 'eq', 'fun': lambda x: x[9] - x[14]},
+]
+
+# Loading the spectrum
+myspec = ds.Spectrum('ngc6300_nuc.fits')
+
 myspec.linefit(
-    p0, fitting_window=(6500, 6700), function='gauss_hermite')
+    p0, fitting_window=(6500, 6700), function='gauss_hermite',
+    constraints=c, bounds=b)
 
 myspec.plotfit()
