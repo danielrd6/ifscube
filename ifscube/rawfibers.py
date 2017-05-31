10d3a94cc89421c3a85638eeafe0df5578aa993f
Daniel Ruschel Dutra <druscheld@gmail.com>
2017-05-31 11:41:30 -0300

    Small changes to the imshow method.

--- ifscube/rawfibers.py
+++ ifscube/rawfibers.py
@@ -22,21 +22,28 @@ class cube:
 
         self._filename = fitsfile
 
-    def imshow(self, data=None, **kwargs):
+    def imshow(self, data=None, ax=None):
 
-        fig = plt.figure(1)
-        plt.clf()
-        ax = fig.add_subplot(111)
+        if ax is None:
+            fig = plt.figure(1)
+            plt.clf()
+            ax = fig.add_subplot(111)
 
         m = self.obj_mask
 
         y, x = self.mdf['yinst'][m], self.mdf['xinst'][m]
 
+        x0 = (x.max() + x.min()) / 2.
+        y0 = (y.max() + y.min()) / 2.
+        
+        x -= x0
+        y -= y0
+
         if data is None:
             data = np.sum(self.data[m], 1)
 
         ax.scatter(x, y, c=data, cmap='inferno', s=120,
-                   marker='H', edgecolor='none', **kwargs)
+                   marker='H', edgecolor='none')
         ax.set_aspect('equal')
 
         plt.show()
