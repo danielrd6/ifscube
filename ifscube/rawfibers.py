5ff35a6bd5f8ab9858ba7a0cd629545d93376a90
Daniel Ruschel Dutra <druscheld@gmail.com>
2017-05-31 10:23:37 -0300

    Fixed some major errors in the imshow method.

--- ifscube/rawfibers.py
+++ ifscube/rawfibers.py
@@ -10,18 +10,33 @@ class cube:
 
         hdu = fits.open(fitsfile)
         self.data = hdu['sci'].data
-        self.mdf = hdu['mdf'].data
+        self._full_mdf = hdu['mdf'].data
+
+        # Valid apertures, which must equal the number of lines
+        # in the data array.
+        apid_mask = self._full_mdf['apid'] != 0
+        self.mdf = self._full_mdf[apid_mask]
+
+        self.sky_mask = self.mdf['beam'] == 0
+        self.obj_mask = self.mdf['beam'] == 1
 
         self._filename = fitsfile
 
-    def imshow(self):
+    def imshow(self, data=None, **kwargs):
 
-        fig = plt.figure()
+        fig = plt.figure(1)
+        plt.clf()
         ax = fig.add_subplot(111)
 
-        y, x = self.mdf['yinst'], self.mdf['xinst']
+        m = self.obj_mask
 
-        pdb.set_trace()
-        ax.scatter(x, y, c=np.sum(self.data, 1), cmap='inferno')
+        y, x = self.mdf['yinst'][m], self.mdf['xinst'][m]
+
+        if data is None:
+            data = np.sum(self.data[m], 1)
+
+        ax.scatter(x, y, c=data, cmap='inferno', s=120,
+                   marker='H', edgecolor='none', **kwargs)
+        ax.set_aspect('equal')
 
         plt.show()
