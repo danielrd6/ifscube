1c338f5b179cfa632d8db8682bea804280c4f16f
druscheld <druscheld@gmail.com>
2017-06-05 12:02:08 -0300

    Updated version and added classifier to the setup file.

--- setup.py
+++ setup.py
@@ -10,14 +10,19 @@ ext1 = Extension(name='ifscube.elprofile',
 packdata = {}
 packdata['ifscube'] = ['examples/*', 'data/*']
 
-setup(name='ifscube',
-      version=__version__,
-      packages=['ifscube'],
-      scripts=['bin/fit_scrutinizer'],
-      description="Fit emssision lines",
-      author="Daniel Ruschel Dutra",
-      author_email="druscheld@gmail.com",
-      url='https://git.cta.if.ufrgs.br/ruschel/ifscube',
-      platform='Linux',
-      license='GPLv3',
-      ext_modules=[ext1])
+setup(
+    name='ifscube',
+    version=__version__,
+    packages=['ifscube'],
+    scripts=['bin/fit_scrutinizer'],
+    description="Fit emssision lines",
+    author="Daniel Ruschel Dutra",
+    author_email="druscheld@gmail.com",
+    url='https://git.cta.if.ufrgs.br/ruschel/ifscube',
+    platform='Linux',
+    license='GPLv3',
+    ext_modules=[ext1],
+    classifiers=[
+      'Programming Language :: Python :: 2.7'
+    ]
+)
