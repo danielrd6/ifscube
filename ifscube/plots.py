7fd5d9513143195b08ef2b85618baa721f87d893
druscheld <druscheld@gmail.com>
2017-12-05 09:38:41 -0200

    Small changes to the w80 plot routine.

--- ifscube/plots.py
+++ ifscube/plots.py
@@ -3,18 +3,28 @@ import ifscube.spectools as st
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
+
 def w80(x):
+
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif', size=14)
 
     # Create figure and axes.
     fig = plt.figure(1)
     plt.clf()
     ax = fig.add_subplot(111)
 
+    # Gets the exponent
+
+    power = np.int(np.log10(np.mean(x[0][3])))
+    w80_label = ('M', 'O') 
+
     for i, j in enumerate(x):
 
         sc = 'C{:d}'.format(i)
+
         # Plot the spectrum.
-        ax.plot(j[2], j[3], c=sc)
+        ax.plot(j[2], j[3] / 10**(power), c=sc)
     
         # Draw w80 lines.
         for k in j[:2]:
@@ -24,8 +34,16 @@ def w80(x):
         ax.axhline(0, ls='dashed', color=sc, alpha=.5)
     
         # Labels.
-        ax.set_xlabel(r'Velocity (km/s)')
-        ax.set_ylabel(r'Normalized flux units')
+        ax.set_xlabel('Velocity (km/s)')
+        ax.set_ylabel(
+            '$F_\lambda$ ($10^{{{:d}}}$ erg s$^{{-1}}$'
+            ' cm$^{{-2}}$ \AA$^{{-1}}$)'.format(power))
+        
+        # Write the W80 values
+        ax.annotate(
+            r'$W_{{80}} (\mathbf{{{:s}}})'
+            ' = {:.2f}$ km/s'.format(w80_label[i], np.float(j[1] - j[0])),
+            xy=(.65, .9 - i / 10), xycoords='axes fraction', size=12)
     
         ax.minorticks_on()
     
