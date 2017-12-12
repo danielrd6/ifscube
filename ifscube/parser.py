8d4b7da58ef53b359f21121e72928fd6770b8499
Daniel Ruschel Dutra <druscheld@gmail.com>
2017-12-12 11:25:18 -0200

    Config parser is now able to handle simple equality constraints of the type parameter = constant.

--- ifscube/parser.py
+++ ifscube/parser.py
@@ -1,4 +1,136 @@
 import configparser
+import copy
+
+
+class ConstraintParser:
+
+    def __init__(self, expr, linefit=None):
+
+        self.linefit = linefit
+        self.expr = expr
+        self._operators = '+-*/><'
+        self._containers = '()[]{}'
+        self._spacer = ' \t\n\r'
+
+        self.__limits__()
+        self.__classify__()
+        self._getConstraintType()
+
+    def __what__(self, c):
+
+        if c.isalpha():
+            group = 'alpha'
+        elif c.isdecimal():
+            group = 'number'
+        elif c in self._operators:
+            group = 'operator'
+        elif c in self._spacer:
+            group = 'spacer'
+        else:
+            group = 'unkonwn'
+
+        return group
+
+    def __limits__(self):
+
+        previous = ''
+        lim = []
+
+        for i, c in enumerate(self.expr):
+
+            current = self.__what__(c)
+            if current != previous:
+                lim += [i]
+                previous = copy.copy(current)
+            else:
+                continue
+
+        self.lim = lim
+
+    def __classify__(self):
+
+        operators = []
+        variables = []
+        containers = []
+        numbers = []
+
+        for c in self.tolist():
+
+            if self.__what__(c) == 'alpha':
+                variables += [c]
+            elif self.__what__(c) == 'number':
+                numbers += [c]
+            elif self.__what__(c) == 'operator':
+                operators += [c]
+            elif self.__what__(c) == 'container':
+                containers += [c]
+            elif self.__what__(c) == 'unknown':
+                continue
+
+        self.numbers = numbers
+        self.operators = operators
+        self.variables = variables
+        self.containers = containers
+
+    def __idx__(self, cname, pname):
+
+        ci = self.linefit.component_names.index(cname)
+        pi = self.linefit.par_names.index(pname)
+        npars = len(self.linefit.par_names)
+
+        idx = ci * npars + pi
+
+        return idx
+
+    def _getConstraintType(self):
+
+        lis = self.tolist()
+
+        if lis[0] in '<>':
+            t = 'ineq'
+        else:
+            t = 'eq'
+
+        self.type = t
+
+    def evaluate(self, component, parameter):
+
+        idx = self.__idx__(component, parameter)
+
+        lis = self.tolist()
+
+        if lis[0] in '<>':
+            lis.remove(lis[0])
+
+        if (lis[0] in self.numbers) and len(lis) == 1:
+
+            a = float(lis[0])
+            def func(x):
+                r = a - x[idx]
+                return r
+
+        # elif (lis[0] in variables) and len(lis) == 1:
+
+        #     a =
+
+        self.constraint = dict(type=self.type, fun=func)
+
+    def tolist(self):
+
+        elements = []
+        lims = self.lim
+
+        for i, j in enumerate(lims):
+           
+            if j == lims[-1]:
+                c = self.expr[lims[i]:None] 
+            else:
+                c = self.expr[lims[i]:lims[i + 1]]
+
+            if c not in self._spacer:
+                elements += [c]
+
+        return elements
 
 
 class LineFitParser:
@@ -24,13 +156,20 @@ class LineFitParser:
         )
 
         self.par_names = par_names[self.cfg['fit']['function']]
+        self.component_names = [
+            i for i in self.cfg.sections() if i not in [
+                'continuum', 'minimization', 'fit']
+        ]
 
         # Each section has to be a line, except for the DEFAULT, MINOPTS,
         # and CONTINUUM sections.
-        for line in self.cfg.sections():
-            if line not in ['continuum', 'minimization', 'fit']:
-                self.component_names += [line]
-                self.parse_line(self.cfg[line])
+        for line in self.component_names:
+            self.parse_line(self.cfg[line])
+
+        for line in self.component_names:
+            for par in self.par_names:
+                prop = self.cfg[line][par].split(',')
+                self._constraints(prop, line, par)
 
         if 'fit' in self.cfg.sections():
             self._fit()
@@ -46,7 +185,7 @@ class LineFitParser:
             self.copts = {}
 
     def _parse_dict(self, section, float_args, int_args, bool_args):
-       
+
         d = {**self.cfg[section]}
 
         for i in float_args:
@@ -58,7 +197,7 @@ class LineFitParser:
                 d[i] = self.cfg.getint(section, i)
 
         for i in bool_args:
-            if i in d: 
+            if i in d:
                 d[i] = self.cfg.getboolean(section, i)
 
         return d
@@ -87,7 +226,7 @@ class LineFitParser:
             fit_opts['fitting_window'] = tuple([float(i) for i in a])
 
         self.fit_opts = fit_opts
-    
+
     def _continuum(self):
 
         self.copts = self._parse_dict(
@@ -106,6 +245,19 @@ class LineFitParser:
         else:
             self.bounds += [(None, None)]
 
+    def _constraints(self, props, component_name, par_name):
+
+        if len(props) > 2:
+            expr = ConstraintParser(props[2], self)
+            expr.evaluate(component_name, par_name)
+
+            import pdb
+            pdb.set_trace()
+
+            self.constraints += [expr.constraint]
+        else:
+            pass
+
     def parse_line(self, line):
 
         for par in self.par_names:
@@ -115,10 +267,11 @@ class LineFitParser:
 
     def get_vars(self):
 
-        d = {**vars(self), **self.fit_opts,}
+        d = {**vars(self), **self.fit_opts}
         todel = [
             'cfg', 'component_names', 'par_names', 'fit_opts', 'copts']
         for i in todel:
             del d[i]
         d['copts'] = self.copts
+        d['constraints'] = self.constraints
         return d
