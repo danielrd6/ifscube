89bfc1050493876aac1822ff7d08a642be5f4060
Daniel <druscheld@gmail.com>
2017-12-15 21:29:01 -0200

    Rearrenging gmosdc
    - The gmosdc class is now only in cubetools for backwards
      compatibility, the actual class has been moved to a new module called
      gmos. Inside gmos there is a class called cube, much like the manga
      class cube.
      This change is part of an effort to make the structure of the modules
      more clear.

--- ifscube/cubetools.py
+++ ifscube/cubetools.py
@@ -4,14 +4,16 @@ Functions for the analysis of integral field spectroscopy.
 Author: Daniel Ruschel Dutra
 Website: https://github.com/danielrd6/ifscube
 """
+# STDLIB
 from copy import deepcopy
 
+# THIRD PARTY
 import numpy as np
 from numpy import ma
 from scipy.integrate import trapz
-from astropy.io import fits
 
-from . import datacube, spectools
+# LOCAL
+from . import gmos
 
 
 def peak_spaxel(cube):
@@ -199,135 +201,6 @@ def scale_bounds(bounds, flux_sf):
     return b
 
 
-class gmosdc(datacube.Cube):
+class gmosdc(gmos.cube):
 
-    """
-    A class for dealing with data cubes, originally written to work
-    with GMOS IFU.
-    """
-
-    def __init__(self, *args, **kwargs):
-
-        if len(args) > 0:
-            self.__load__(*args, **kwargs)
-
-    def __load__(self, fitsfile, redshift=None, vortab=None,
-                 dataext=1, hdrext=0, var_ext=None, ncubes_ext=None,
-                 nan_spaxels='all', spatial_mask=None):
-        """
-        Initializes the class and loads basic information onto the
-        object.
-
-        Parameters:
-        -----------
-        fitstile : string
-            Name of the FITS file containing the GMOS datacube. This
-            should be the standard output from the GFCUBE task of the
-            GEMINI-GMOS IRAF package.
-        redshift : float
-            Value of redshift (z) of the source, if no Doppler
-            correction has been applied to the spectra yet.
-        vortab : string
-            Name of the file containing the Voronoi binning table
-        dataext: integer
-            Extension of the FITS file containing the scientific data.
-        hdrext: integer
-            Extension of the FITS file containing the basic header.
-        var_ext: integer
-            Extension of the FITS file containing the variance cube.
-        nan_spaxels: None, 'any', 'all'
-            Mark spaxels as NaN if any or all pixels are equal to
-            zero.
-
-
-        Returns:
-        --------
-        Nothing.
-        """
-
-        self.dataext = dataext
-        self.var_ext = var_ext
-        self.ncubes_ext = ncubes_ext
-        self.spatial_mask = spatial_mask
-
-        hdulist = fits.open(fitsfile)
-
-        self.data = hdulist[dataext].data
-
-        if nan_spaxels is not None:
-            if nan_spaxels == 'all':
-                self.nanSpaxels = np.all(self.data == 0, 0)
-            if nan_spaxels == 'any':
-                self.nanSapxels = np.any(self.data == 0, 0)
-            self.data[:, self.nanSpaxels] = np.nan
-
-        self.header_data = hdulist[dataext].header
-        self.header = hdulist[hdrext].header
-        self.hdrext = hdrext
-
-        self.wl = spectools.get_wl(
-            fitsfile, hdrext=dataext, dimension=0, dwlkey='CD3_3',
-            wl0key='CRVAL3', pix0key='CRPIX3')
-
-        if redshift is None:
-            try:
-                redshift = self.header['REDSHIFT']
-            except KeyError:
-                print(
-                    'WARNING! Redshift not given and not found in the image' +
-                    ' header. Using redshift = 0.')
-                redshift = 0.0
-        self.restwl = self.wl / (1. + redshift)
-
-        if var_ext is not None:
-            # The noise for each pixel in the cube
-            self.noise_cube = hdulist[var_ext].data
-            self.variance = np.square(self.noise_cube)
-
-            # An image of the mean noise, collapsed over the
-            # wavelength dimension.
-            self.noise = np.nanmean(hdulist[var_ext].data, 0)
-
-            # Image of the mean signal
-            self.signal = np.nanmean(self.data, 0)
-
-            # Maybe this step is redundant, I have to check it later.
-            # Guarantees that both noise and signal images have
-            # the appropriate spaxels set to nan.
-            self.noise[self.nanSpaxels] = np.nan
-            self.signal[self.nanSpaxels] = np.nan
-
-            self.noise[np.isinf(self.noise)] =\
-                self.signal[np.isinf(self.noise)]
-        else:
-            self.variance = np.ones_like(self.data)
-
-        if ncubes_ext is not None:
-            # The self.ncubes variable describes how many different
-            # pixels contributed to the final combined pixel. This can
-            # also serve as a flag, when zero cubes contributed to the
-            # pixel. Additionaly, it may be useful to mask regions that
-            # are present in only one observation, for greater
-            # confidence.
-            self.ncubes = hdulist[ncubes_ext].data
-        else:
-            self.ncubes = np.ones_like(self.data)
-
-        self.flags = np.zeros_like(self.data)
-        self.flags[self.ncubes <= 0] = 1
-
-        try:
-            if self.header['VORBIN']:
-                vortab = fits.open(fitsfile)['VOR'].data
-                self.voronoi_tab = vortab
-                self.binned = True
-        except KeyError:
-            self.binned = False
-
-        self.fitsfile = fitsfile
-        self.redshift = redshift
-
-        self.stellar = np.zeros_like(self.data)
-        self.weights = np.zeros_like(self.data)
-
-        self.__set_spec_indices__()
+    pass
