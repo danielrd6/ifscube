4b146c6aa27ade9b0cfd860100e5c750691e9d6a
Daniel Ruschel Dutra <druscheld@gmail.com>
2018-04-11 23:13:59 -0300

    Added default values for var_ext and ncubes_ext in the gmos module.

--- ifscube/gmos.py
+++ ifscube/gmos.py
@@ -21,8 +21,8 @@ class cube(datacube.Cube):
             self.__load__(*args, **kwargs)
 
     def __load__(self, fitsfile, redshift=None, vortab=None,
-                 dataext='SCI', hdrext='PRIMARY', var_ext=None,
-                 ncubes_ext=None, nan_spaxels='all',
+                 dataext='SCI', hdrext='PRIMARY', var_ext='ERR',
+                 ncubes_ext='NCUBE', nan_spaxels='all',
                  spatial_mask=None):
         """
         Initializes the class and loads basic information onto the
