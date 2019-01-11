0d7d9c7908a7c4a5cbbc4bb5372c9df02711e076
Daniel Ruschel Dutra <druscheld@gmail.com>
2019-01-11 13:22:41 -0200

    Fixed another example.

--- ifscube/examples/single_fit_gh.py
+++ ifscube/examples/single_fit_gh.py
@@ -14,38 +14,39 @@ z = 0.0037
 
 # Defining the initial guess for the parameters.
 
+# The number of components is the number of spectral features to be fit.
 ncomponents = 3
-npars = 5  # five parameters for the gauss hermite polynomials
-           # flux, wavelength, sigma, h3, h4
+# Five parameters for the gauss hermite polynomial amplitude, velocity, sigma, h3, h4
+npars = 5
 
 p0 = np.zeros(ncomponents * npars)
 
-p0[0::5] = 1e-14                # flux
-p0[1::5] = lines_wl * (1. + z)  # wavelength
-p0[2::5] = 3.0                  # sigma
-p0[3::5] = 0.0                  # h3
-p0[4::5] = 0.0                  # h4
+p0[0::5] = 1e-14    # amplitude
+p0[1::5] = 0.0      # velocity
+p0[2::5] = 100.0    # sigma
+p0[3::5] = 0.0      # h3
+p0[4::5] = 0.0      # h4
 
 # Setting bounds
 
 b = []
 for i in range(ncomponents):
-    b += [[0, 1e-12]]                                                  # flux
-    b += [[lines_wl[i] * (1. + z) - 10, lines_wl[i] * (1. + z) + 10]]  # wl
-    b += [[1.5, 9]]                                                    # sigma
-    b += [[-.2, +.2]]                                                  # h3
-    b += [[-.2, +.2]]                                                  # h4
+    b += [[0, 1e-12]]        # flux
+    b += [[-500.0, +500.0]]  # wl
+    b += [[40.0, 500.0]]     # sigma
+    b += [[-.2, +.2]]        # h3
+    b += [[-.2, +.2]]        # h4
 
 # Setting the constraints
 
 c = [
     # Keeping the same doppler shift on all lines
-    {'type': 'eq', 'fun': lambda x: x[1]/lines_wl[0] - x[6]/lines_wl[1]},
-    {'type': 'eq', 'fun': lambda x: x[6]/lines_wl[1] - x[11]/lines_wl[2]},
+    {'type': 'eq', 'fun': lambda x: x[1] - x[6]},
+    {'type': 'eq', 'fun': lambda x: x[6] - x[11]},
 
     # And the same goes for the sigmas
-    {'type': 'eq', 'fun': lambda x: x[2]/x[1] - x[7]/x[6]},
-    {'type': 'eq', 'fun': lambda x: x[7]/x[6] - x[12]/x[11]},
+    {'type': 'eq', 'fun': lambda x: x[2] - x[7]},
+    {'type': 'eq', 'fun': lambda x: x[7] - x[12]},
 
     # All the same h3's and h4's
     {'type': 'eq', 'fun': lambda x: x[3] - x[8]},
@@ -58,7 +59,7 @@ c = [
 myspec = ds.Spectrum('ngc6300_nuc.fits')
 
 myspec.linefit(
-    p0, fitting_window=(6500, 6700), function='gauss_hermite',
-    constraints=c, bounds=b)
+    p0, fitting_window=(6500.0, 6700.0), feature_wl=lines_wl, function='gauss_hermite', constraints=c, bounds=b,
+    fit_continuum=True)
 
 myspec.plotfit()
