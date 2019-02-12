71c8b230323c3dcbdf311adc6a52a0a4048de447
Daniel Ruschel Dutra <druscheld@gmail.com>
2019-02-12 08:21:44 -0200

    Refactoring.

--- ifscube/stats.py
+++ ifscube/stats.py
@@ -4,33 +4,34 @@
 import numpy as np
 
 
-def line_flux_error(flux, fwhm, delta_l, peak, s_peak):
+def line_flux_error(flux, fwhm, sampling_interval, amplitude, amplitude_error):
     """
     Evaluates the uncertainty in the flux of the fitted line.
     Based on Lenz & Ayres 1992 and Wesson 2015
 
     Parameters
     ----------
-    flux : number
+    flux : float
       Integrated flux in the line.
-    fwhm : number
+    fwhm : float
       Full width at half maximum.
-    delta_l : number
+    sampling_interval : float
       Sampling step in wavelength, usually in angstroms per pixel.
-    peak : number
+    amplitude : float
       The peak value of the flux.
-    s_peak : number
+    amplitude_error : float
       Uncertainty in the peak flux.
 
 
     Returns
     -------
-    s_flux : number
+    flux_error : number
       Uncertainty in the integrated flux of the line.
     """
 
+    # FIXME: What is this?
     cx = 0.67
 
-    s_flux = flux / (cx * np.sqrt(fwhm / delta_l) * (peak / s_peak))
+    flux_error = flux / (cx * np.sqrt(fwhm / sampling_interval) * (amplitude / amplitude_error))
 
-    return s_flux
+    return flux_error
