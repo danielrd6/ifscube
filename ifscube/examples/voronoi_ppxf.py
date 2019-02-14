3d0f34123b2d774b5867031ef683c53609bc58d1
Daniel Ruschel Dutra <druscheld@gmail.com>
2019-02-14 09:07:20 -0200

    Formatting changes.

--- ifscube/examples/voronoi_ppxf.py
+++ ifscube/examples/voronoi_ppxf.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
-# stdlib
 import glob
-import site
 
-# third party
-from astropy.io import fits
 import numpy as np
+from astropy.io import fits
 
-# local
 from ifscube import Cube, spectools
 
 
 def load_models(searchPattern):
-
     baseFiles = glob.glob(searchPattern)
     baseFiles.sort()
 
@@ -27,11 +22,9 @@ def load_models(searchPattern):
 
 
 if __name__ == '__main__':
-
     #
     # Voronoi binning
     #
-
     # Loads the datacube.
     mycube = Cube('ngc3081_cube.fits', variance='ERR', scidata='SCI')
 
@@ -39,33 +32,13 @@ if __name__ == '__main__':
     mycube.snr_eval(wl_range=(5650, 5750))
 
     # Actual binning.
-    mycube.voronoi_binning(
-        target_snr=3, write_fits=True, overwrite=True, plot=False)
+    mycube.voronoi_binning(target_snr=3, write_fits=True, overwrite=True, plot=False)
 
     #
     # pPXF fitting
     #
-
-    # Loads base files for pPXF.
-    #
-    # ATTENTION: You must modify this path to match your local copy
-    # of the miles models. If you have installed the pPXF python
-    # package via pip, everything from site-packages on should be
-    # the same.
-    wl, base = load_models(
-        site.getsitepackages()[0] + '/ppxf/miles_models/Mun*fits')
-
     # Loads the Voronoi binned cube.
-    mycube = Cube('ngc3081_cube.bin.fits', variance='ERR', scidata='SCI',
-        vortab='VORTAB')
+    mycube = Cube('ngc3081_cube.bin.fits', variance='ERR', scidata='SCI', vortab='VORTAB')
 
     # Actual pPXF fitting.
-    mycube.ppxf_kinematics(
-        fitting_window=(5650, 5850),
-        base_wl=wl,
-        base_spec=base,
-        base_cdelt=0.9,
-        write_fits=True,
-        deg=3,
-        overwrite=True,
-    )
+    mycube.ppxf_kinematics(fitting_window=(5650, 5850), write_fits=True, deg=3, overwrite=True)
