bce98e32da0d5965c6718908f5a74942fe697cf7
druscheld <druscheld@gmail.com>
2019-06-20 11:27:11 -0300

    Added example for channel maps.

---
+++ ifscube/examples/channel_maps.py
@@ -0,0 +1,6 @@
+from ifscube import Cube
+
+if __name__ == '__main__':
+    a = Cube('ngc3081_cube.fits')
+    c_opts = {'degr': 1, 'lower_threshold': 3, 'upper_threshold': 3}
+    a.channel_maps(6564.0, -500.0, +500.0, log_flux=True, continuum_options=c_opts)
