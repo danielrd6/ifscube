88451e93ff4078cf7eeddaa7b57b0a84c2c7302a
druscheld <druscheld@gmail.com>
2019-07-02 10:45:26 -0300

    Changed from gmos to Cube, and appended configuration file.

--- ifscube/examples/cube_fit_gaussian_cfg.py
+++ ifscube/examples/cube_fit_gaussian_cfg.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
-# THIRD PARTY
-from astropy.io import fits
-
-# LOCAL
-from ifscube import gmos, parser
+from ifscube import Cube, parser
+from ifscube.cubetools import append_config
 
 if __name__ == '__main__':
 
     z = 0.0081
 
-    mycube = gmos.Cube('ngc3081_cube.fits', redshift=z, var_ext=None, ncubes_ext=None)
+    my_cube = Cube('ngc3081_cube.fits', redshift=z)
 
+    # Loads the configuration file and stores the parameters
+    # in the line_fit_args dict.
     c = parser.LineFitParser('halpha_cube.cfg')
-    linefit_args = c.get_vars()
+    line_fit_args = c.get_vars()
 
-    mycube.linefit(**linefit_args)
+    my_cube.linefit(**line_fit_args)
+
+    # Append the input configuration file to the output of the fit.
+    append_config(config_file='halpha_cube.cfg', fit_file='ngc3081_cube_linefit.fits')
 
     # Plots the fit for the spaxel defined in "idx".
-    mycube.plotfit(3, 3)
+    my_cube.plotfit(3, 3)
