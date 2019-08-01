b326a98089f0f62e97c4605ac017b580e25095ae
druscheld <druscheld@gmail.com>
2019-08-01 00:58:23 -0300

    Constrained the useless results of w80.

--- ifscube/spectools.py
+++ ifscube/spectools.py
@@ -687,19 +687,19 @@ def w80eval(wl: np.ndarray, spec: np.ndarray, wl0: float, smooth: float = None) 
                                         equivalencies=units.doppler_relativistic(wl0 * units.angstrom))
 
     if not (spec == 0.0).all() and not np.isnan(spec).all():
+        y = ma.masked_invalid(spec)
         if smooth is not None:
             kernel = Gaussian1DKernel(smooth)
-            y = convolve(ma.masked_invalid(spec), kernel=kernel, boundary='extend')
-        else:
-            y = ma.masked_invalid(spec)
+            y = convolve(y, kernel=kernel, boundary='extend')
 
-        cumulative = cumtrapz(y.data[~y.mask], velocity.value[~y.mask])
+        cumulative = cumtrapz(y[~y.mask], velocity[~y.mask], initial=0)
         cumulative /= cumulative.max()
 
-        r0, r1 = [velocity[(np.abs(cumulative - k)).argsort()[0]].value for k in (0.1, 0.9)]
+        r0 = velocity[(np.abs(cumulative - 0.1)).argsort()[0]].value
+        r1 = velocity[(np.abs(cumulative - 0.9)).argsort()[0]].value
         w80 = r1 - r0
     else:
-        w80, r0, r1 = 3 * (np.nan,)
+        w80, r0, r1 = np.nan, np.nan, np.nan
 
     return w80, r0, r1, velocity, spec
 
