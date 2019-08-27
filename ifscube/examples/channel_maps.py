9556bfae8cce9a2c14dea63aedbd91e554dc153a
Daniel Ruschel Dutra <druscheld@gmail.com>
2019-08-27 15:48:17 -0300

    Updated channel maps example storing returned variables.

--- ifscube/examples/channel_maps.py
+++ ifscube/examples/channel_maps.py
@@ -1,6 +1,7 @@
 from ifscube import Cube
+from ifscube.channel_maps import channelmaps
 
 if __name__ == '__main__':
     a = Cube('ngc3081_cube.fits')
     c_opts = {'degree': 1, 'lower_threshold': 3, 'upper_threshold': 3}
-    a.channel_maps(6564.0, -500.0, +500.0, log_flux=True, continuum_options=c_opts)
+    cm = channelmaps(a, 6564.0, -500.0, +500.0, log_flux=True, continuum_options=c_opts)
