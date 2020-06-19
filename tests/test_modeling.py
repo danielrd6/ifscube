19723b6c1bc7605b0eb4fe3ad22aa21d964f917a
druscheld <druscheld@gmail.com>
2020-06-19 15:34:58 -0300

    Bounds working.

--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -13,7 +13,7 @@ def test_simple_fit():
     fit.add_feature(name='n2_6548', rest_wavelength=6548.04, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
     fit.add_feature(name='ha', rest_wavelength=6562.8, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
     fit.add_feature(name='n2_6583', rest_wavelength=6583.46, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
-    fit.fit()
+    fit.fit(verbose=True)
     fit.plot()
     assert 1
 
@@ -23,6 +23,18 @@ def test_skip_feature():
     fit = modeling.LineFit(spec, fitting_window=(6400.0, 6700.0), fit_continuum=True)
     fit.add_feature(name='hb', rest_wavelength=4861.0, amplitude=0, velocity=0, sigma=10)
     fit.add_feature(name='ha', rest_wavelength=6562.8, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
+    fit.fit()
+    fit.plot()
+    assert 1
+
+
+@pytest.mark.filterwarnings("ignore:RADECSYS", "ignore:'datfix'")
+def test_bounds():
+    fit = modeling.LineFit(spec, fitting_window=(6400.0, 6700.0), fit_continuum=True)
+    fit.add_feature(name='hb', rest_wavelength=4861.0, amplitude=0, velocity=0, sigma=10)
+    fit.add_feature(name='ha', rest_wavelength=6562.8, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
+    fit.set_bounds('ha', 'velocity', [-50, 50])
+    fit.set_bounds('ha', 'amplitude', [None, 0.4e-14])
     fit.fit()
     fit.plot()
     assert 1
