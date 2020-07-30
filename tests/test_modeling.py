dbb7d67a1de4b51b46bbb5f6b581463e477a544e
druscheld <druscheld@gmail.com>
2020-07-30 15:34:46 -0300

    Updated tests for new fit_continuum option

--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -4,11 +4,17 @@ import pytest
 from ifscube import onedspec, modeling, datacube
 
 
-def simple_fit(function: str = 'gaussian'):
-    file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc6300_nuc.fits')
-    spec = onedspec.Spectrum(file_name)
-    fit = modeling.LineFit(spec, function=function, fitting_window=(6400.0, 6700.0))
-    fit.fit_continuum()
+def simple_fit(function: str = 'gaussian', fit_type: str = 'spectrum'):
+    if fit_type == 'spectrum':
+        file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc6300_nuc.fits')
+        spec = onedspec.Spectrum(file_name)
+        fit = modeling.LineFit(spec, function=function, fitting_window=(6400.0, 6700.0))
+    elif fit_type == 'cube':
+        file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc3081_cube.fits')
+        cube = datacube.Cube(file_name)
+        fit = modeling.LineFit3D(cube, function=function, fitting_window=(6400.0, 6700.0))
+    else:
+        raise RuntimeError(f'fit_type "{fit_type}" not understood.')
     fit.add_feature(name='n2_6548', rest_wavelength=6548.04, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
     fit.add_feature(name='ha', rest_wavelength=6562.8, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
     fit.add_feature(name='n2_6583', rest_wavelength=6583.46, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
@@ -19,7 +25,6 @@ def full_fit(function: str = 'gaussian'):
     file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc6300_nuc.fits')
     spec = onedspec.Spectrum(file_name)
     fit = modeling.LineFit(spec, function=function, fitting_window=(6400.0, 6800.0))
-    fit.fit_continuum()
     names = ['n2_6548', 'ha', 'n2_6583', 's2_6716', 's2_6731']
     r_wl = [6548.04, 6562.8, 6583.46, 6716.44, 6730.86]
 
@@ -44,13 +49,13 @@ def full_fit(function: str = 'gaussian'):
 
 def test_simple_fit():
     fit = simple_fit()
-    fit.fit(verbose=True)
+    fit.fit(verbose=True, fit_continuum=True)
     assert 1
 
 
 def test_flux():
     fit = simple_fit()
-    fit.fit()
+    fit.fit(fit_continuum=True)
     fit.integrate_flux(sigma_factor=5.0)
     assert 1
 
@@ -58,7 +63,7 @@ def test_flux():
 def test_optimize_fit():
     fit = simple_fit()
     fit.optimize_fit(width=5.0)
-    fit.fit()
+    fit.fit(fit_continuum=True)
     assert 1
 
 
@@ -74,11 +79,10 @@ def test_skip_feature():
     file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc6300_nuc.fits')
     spec = onedspec.Spectrum(file_name)
     fit = modeling.LineFit(spec, fitting_window=(6400.0, 6700.0))
-    fit.fit_continuum()
     with pytest.warns(UserWarning):
         fit.add_feature(name='hb', rest_wavelength=4861.0, amplitude=0, velocity=0, sigma=10)
     fit.add_feature(name='ha', rest_wavelength=6562.8, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
-    fit.fit()
+    fit.fit(fit_continuum=True)
     assert 1
 
 
@@ -86,7 +90,7 @@ def test_bounds():
     fit = simple_fit()
     fit.set_bounds('ha', 'velocity', [-50, 50])
     fit.set_bounds('ha', 'amplitude', [None, 0.4e-14])
-    fit.fit()
+    fit.fit(fit_continuum=True)
     assert 1
 
 
@@ -97,7 +101,7 @@ def test_constraints():
     fit.add_minimize_constraint('n2_6548.amplitude', 'n2_6583.amplitude / 3.06')
     fit.add_minimize_constraint('n2_6548.amplitude', '< ha.amplitude')
     fit.add_minimize_constraint('ha.amplitude', '< 3.5e-15')
-    fit.fit()
+    fit.fit(fit_continuum=True)
     assert fit._get_feature_parameter('ha', 'amplitude', 'solution') < 3.5e-15
 
 
@@ -105,14 +109,14 @@ def test_gauss_hermite():
     fit = simple_fit(function='gauss_hermite')
     fit.add_minimize_constraint(parameter='n2_6548.h_3', expression='n2_6583.h_3')
     fit.add_minimize_constraint(parameter='n2_6548.h_4', expression='n2_6583.h_4')
-    fit.fit()
+    fit.fit(fit_continuum=True)
     assert 1
 
 
 def test_kinematic_groups():
     fit = full_fit()
     fit.optimize_fit(width=5.0)
-    fit.fit()
+    fit.fit(fit_continuum=True)
 
     assert 1
 
@@ -121,32 +125,26 @@ def test_fixed_features():
     file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc6300_nuc.fits')
     spec = onedspec.Spectrum(file_name)
     fit = modeling.LineFit(spec, function='gaussian', fitting_window=(6400.0, 6800.0))
-    fit.fit_continuum()
     names = ['n2_6548', 'ha', 'n2_6583', 's2_6716', 's2_6731']
     r_wl = [6548.04, 6562.8, 6583.46, 6716.44, 6730.86]
 
     for name, wl, k in zip(names, r_wl, [0, 1, 0, 2, 2]):
         fit.add_feature(name=name, rest_wavelength=wl, amplitude=1.0e-14, velocity=0.0, sigma=100.0,
                         fixed='n2' in name, kinematic_group=k)
-
+    fit.fit(fit_continuum=True)
     assert 1
 
 
 def test_monte_carlo():
     fit = full_fit()
     fit.optimize_fit(width=5.0)
-    fit.fit()
+    fit.fit(fit_continuum=True)
     fit.monte_carlo(3)
 
     assert 1
 
 
 def test_simple_cube_fit():
-    file_name = pkg_resources.resource_filename('ifscube', 'examples/ngc3081_cube.fits')
-    cube = datacube.Cube(file_name)
-    fit = modeling.LineFit3D(cube, function='gaussian', fitting_window=(6400.0, 6700.0))
-    fit.add_feature(name='n2_6548', rest_wavelength=6548.04, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
-    fit.add_feature(name='ha', rest_wavelength=6562.8, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
-    fit.add_feature(name='n2_6583', rest_wavelength=6583.46, amplitude=1.0e-14, velocity=0.0, sigma=100.0)
-    fit.fit()
+    fit = simple_fit(fit_type='cube')
+    fit.fit(fit_continuum=True)
     assert True
