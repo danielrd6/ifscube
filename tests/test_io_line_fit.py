22b95c07709b7b0e0a9e9b00d6ed57b02ffe2542
druscheld <druscheld@gmail.com>
2020-07-31 12:10:02 -0300

    Added test for writing fit output.

--- tests/test_io_line_fit.py
+++ tests/test_io_line_fit.py
@@ -3,6 +3,8 @@ import pkg_resources
 from astropy import table
 from ifscube.io import line_fit
 
+from .test_modeling import simple_fit
+
 
 def test_parse_config_table():
     file_name = pkg_resources.resource_filename('ifscube', 'examples/example_onedspec_linefit.fits')
@@ -14,4 +16,11 @@ def test_parse_config_table():
 def test_load_fit():
     file_name = pkg_resources.resource_filename('ifscube', 'examples/example_onedspec_linefit.fits')
     line_fit.load_fit(file_name)
+    assert 1
+
+
+def test_write_fit_1d():
+    fit = simple_fit()
+    fit.fit(verbose=True, fit_continuum=True)
+    line_fit.write_spectrum_fit(fit, out_image='tests/test_write_fit_1d.fits', function='gaussian', overwrite=True)
     assert 1
