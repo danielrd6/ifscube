cd4dced2ff2395bb2f536db4bfe1d113117fe868
danielrd6 <daniel@astro.ufsc.br>
2022-02-01 09:28:10 -0300

    Changed version information in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from numpy.distutils.core import Extension, setup
 
-with open('./ifscube/.version', 'r') as version_file:
-    __version__ = version_file.read().strip('\n')
-
 ext1 = Extension(name='ifscube.elprofile', sources=['ifscube/profiles.f90'])
 
 package_data = {
@@ -17,7 +14,7 @@ package_data = {
 setup(
     name='ifscube',
     python_requires='>=3.7',
-    version=__version__,
+    version="1.1",
     packages=['ifscube', 'ifscube.io'],
     package_data=package_data,
     scripts=['bin/fit_scrutinizer', 'bin/cubefit', 'bin/specfit', 'bin/fit_rotation'],
