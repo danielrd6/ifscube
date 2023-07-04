10a86e36762b5b763a36e801336686a0e59b5b93
Daniel Ruschel Dutra <druscheld@gmail.com>
2023-07-04 16:12:48 -0300

    Changed assert statements and added tests
    The previous version of the assertion statement required the array to have a strictly ascending orientation.
    
        fitting_window[0] > wavelength[-1]
    
    instead of the orientation agnostic approach.
    
        fitting_window[0] > np.max(wavelength)
    
    I guess nobody in their right mind would try to declare the fitting window with the upper limit first.

--- ifscube/fitter.py
+++ ifscube/fitter.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from typing import Union
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -30,11 +31,11 @@ def clear_lock(lock_name):
 def spectrum_fit(data: Union[Cube, onedspec.Spectrum], **line_fit_args):
     fit = ifscube.io.line_fit.setup_fit(data, **line_fit_args)
 
-    assert fit.fitting_window[0] < fit.wavelength[-1], \
-        f"Lower limit of fitting window above maximum wavelength: {fit.fitting_window[0]} > {fit.wavelength[-1]}"
+    assert fit.fitting_window[0] < np.max(fit.wavelength), \
+        f"Lower limit of fitting window above maximum wavelength: {fit.fitting_window[0]} >= {np.max(fit.wavelength)}"
 
-    assert fit.fitting_window[1] > fit.wavelength[0], \
-        f"Upper limit of fitting window below minimum wavelength: {fit.fitting_window[1]} < {fit.wavelength[0]}"
+    assert fit.fitting_window[1] > np.min(fit.wavelength), \
+        f"Upper limit of fitting window below minimum wavelength: {fit.fitting_window[1]} <= {np.min(fit.wavelength)}"
 
     if line_fit_args['fit_continuum']:
         continuum_options = line_fit_args['copts'] if line_fit_args['copts'] is not None else {}
---
+++ tests/test_fitter.py
@@ -0,0 +1,41 @@
+import importlib.resources
+
+import numpy as np
+import pytest
+
+from ifscube import parser
+from ifscube.fitter import spectrum_fit
+from ifscube.onedspec import Spectrum
+
+
+def basic_1d_fit():
+    config = importlib.resources.files("ifscube") / "examples/halpha_gauss.cfg"
+    input_data = importlib.resources.files("ifscube") / "examples/manga_onedspec.fits"
+    data = Spectrum(fname=str(input_data), scidata="F_OBS", primary="PRIMARY")
+
+    c = parser.LineFitParser(str(config))
+    line_fit_args = c.get_vars()
+
+    return data, line_fit_args
+
+
+def test_wavelength_window_low():
+    data, line_fit_args = basic_1d_fit()
+    max_wl = np.max(data.rest_wavelength)
+
+    line_fit_args["fitting_window"] = (max_wl * 1.5, max_wl * 2.0)
+    line_fit_args["optimize_fit"] = False
+
+    with pytest.raises(AssertionError, match="Lower limit of fitting window above maximum"):
+        spectrum_fit(data, **line_fit_args)
+
+
+def test_wavelength_window_high():
+    data, line_fit_args = basic_1d_fit()
+    min_wl = np.min(data.rest_wavelength)
+
+    line_fit_args["fitting_window"] = (min_wl * 0.5, min_wl * 0.8)
+    line_fit_args["optimize_fit"] = False
+
+    with pytest.raises(AssertionError, match="Upper limit of fitting window below minimum"):
+        spectrum_fit(data, **line_fit_args)
