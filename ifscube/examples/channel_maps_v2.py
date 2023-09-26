cb15d80125305eae647ed3f4863c83e229b93db1
danielrd6 <daniel@astro.ufsc.br>
2023-09-26 13:05:42 -0300

    Changed channel maps example to also load a previous result.

--- ifscube/examples/channel_maps_v2.py
+++ ifscube/examples/channel_maps_v2.py
@@ -1,20 +1,29 @@
+import pickle
+
+from astropy.units import Quantity
+
 from ifscube import Cube
 from ifscube import channel_maps
-from astropy import units
-from astropy.units import Quantity
 
 
 def main():
-    fw = Quantity(value=[4900, 5100], unit="Angstrom")
-    cube = Cube("/home/daniel/repos/channel_outflows/test_galaxy.fits")
-    cm = channel_maps.ChannelMaps(cube, fitting_window=fw)
+    evaluate_maps = False
+    if evaluate_maps:
+        fw = Quantity(value=[4900, 5100], unit="Angstrom")
+        cube = Cube("/home/daniel/repos/channel_outflows/test_galaxy.fits")
+        cm = channel_maps.ChannelMaps(cube, fitting_window=fw)
 
-    reference_wavelength = Quantity(value=5006.843, unit="Angstrom")
-    vel_min = Quantity(value=-1000, unit="km / s")
-    vel_max = Quantity(value=+1000, unit="km / s")
-    channels = 10
+        reference_wavelength = Quantity(value=5006.843, unit="Angstrom")
+        vel_min = Quantity(value=-1000, unit="km / s")
+        vel_max = Quantity(value=+1000, unit="km / s")
+        channels = 10
 
-    cm.evaluate_channel_maps(lambda0=reference_wavelength, vel_min=vel_min, vel_max=vel_max, channels=channels)
+        cm.evaluate_channel_maps(lambda0=reference_wavelength, vel_min=vel_min, vel_max=vel_max, channels=channels)
+        with open("cm.pickle", "wb") as f:
+            pickle.dump(cm, f)
+    else:
+        with open("cm.pickle", "rb") as f:
+            cm = pickle.load(f)
 
     cm.plot_spectrum(25, 15)
 
