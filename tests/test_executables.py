7efcd097bd728812b656158f886f0f96e965aa26
Daniel <daniel.ruschel@ufsc.br>
2023-10-02 07:39:40 -0300

    Executable tests are more independent of installation
    The previous version of the test_executables.py script required the specfit and cubefit scripts to be present at the PATH, and also be executable by the OS. While this worked fine for typical Linux systems, it failed on Windows because the lack of extension on the script name made the OS unsure on how to run it.
    
    In this version the subprocess.run method invokes the python interpreter explicitly, and passes the absolute path to the script being tested.

--- tests/test_executables.py
+++ tests/test_executables.py
@@ -1,16 +1,19 @@
 import importlib.resources
+import os
 import subprocess
 
 
 def test_specfit():
     config = importlib.resources.files("ifscube") / "examples/halpha_gauss.cfg"
     input_data = importlib.resources.files("ifscube") / "examples/manga_onedspec.fits"
-    subprocess.run(["specfit", "-o", "-c", config, input_data])
+    script = os.path.abspath(os.path.join(str(importlib.resources.files("ifscube")), "..", "bin", "specfit"))
+    subprocess.run(["python", script, "-o", "-c", config, input_data])
     assert 1
 
 
 def test_cubefit():
     config = importlib.resources.files("ifscube") / "examples/halpha_cube.cfg"
     input_data = importlib.resources.files("ifscube") / "examples/ngc3081_cube.fits"
-    subprocess.run(["cubefit", "-o", "-c", config, input_data])
+    script = os.path.abspath(os.path.join(str(importlib.resources.files("ifscube")), "..", "bin", "cubefit"))
+    subprocess.run(["python", script, "-o", "-c", config, input_data])
     assert 1
