1c8ecbdc22d464a68fb1ccc6881a08330edd498e
Daniel Ruschel Dutra <daniel.ruschel@ufsc.br>
2024-05-03 13:35:20 -0300

    C extension for gauss_vel is now fully functional.
    Additionaly, I double checked all the math.

--- ifscube/profiles.c
+++ ifscube/profiles.c
@@ -3,38 +3,22 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>  // Include NumPy header
 
-
 static PyObject *gauss_vel(PyObject *self, PyObject *args) {
-    PyObject *wl_obj;  // Input Python object for the wavelength
-    PyObject *rest_wl_obj;  // Input Python object for the wavelength
-    PyObject *p_obj;  // Input Python object for the parameters
+    PyObject *wl_obj;  // Input Python object for the wavelength vector
+    PyObject *rest_wl_obj;  // Input Python object for the rest wavelength of spectral features
+    PyObject *p_obj;  // Input Python object for the function parameters
     PyArrayObject *wl_array;  // NumPy array for the wavelength
-    PyArrayObject *rest_wl_array;  // NumPy array for the wavelength
+    PyArrayObject *rest_wl_array;  // NumPy array for the rest wavelength
     PyArrayObject *p_array;  // NumPy array for the parameters
 
     // Parse the input arguments and specify "O" for a generic Python object
-    if (!PyArg_ParseTuple(args, "OOO", &wl_obj, &p_obj, &rest_wl_obj))
+    if (!PyArg_ParseTuple(args, "OOO", &wl_obj, &rest_wl_obj, &p_obj))
         return NULL;
 
     // Convert the input object to a NumPy array with any data type and flags
     wl_array = (PyArrayObject *)PyArray_FromAny(wl_obj, NULL, 0, 0, NPY_ARRAY_ENSUREARRAY, NULL);
     rest_wl_array = (PyArrayObject *)PyArray_FromAny(rest_wl_obj, NULL, 0, 0, NPY_ARRAY_ENSUREARRAY, NULL);
     p_array = (PyArrayObject *)PyArray_FromAny(p_obj, NULL, 0, 0, NPY_ARRAY_ENSUREARRAY, NULL);
-
-    if (wl_array == NULL) {
-        PyErr_SetString(PyExc_TypeError, "Failed to convert input to NumPy array");
-        return NULL;
-    }
-
-    if (rest_wl_array == NULL) {
-        PyErr_SetString(PyExc_TypeError, "Failed to convert input to NumPy array");
-        return NULL;
-    }
-
-    if (p_array == NULL) {
-        PyErr_SetString(PyExc_TypeError, "Failed to convert input to NumPy array");
-        return NULL;
-    }
 
     // Get pointers to the data in the NumPy array
     double *wavelength = (double *)PyArray_DATA(wl_array);
@@ -44,33 +28,25 @@ static PyObject *gauss_vel(PyObject *self, PyObject *args) {
     npy_intp rest_wl_size = PyArray_SIZE(rest_wl_array);
     npy_intp p_size = PyArray_SIZE(p_array);
 
-    // Print array contents
-    /*
-    printf("Array contents:\n");
-    for (npy_intp i = 0; i < wl_size; i++) {
-        printf("%e ", wavelength[i]);
-    }
-    printf("\n");
-    */
-
     double *flux = malloc(wl_size * sizeof(double));
     double lam_ratio, vel, w;
     const double c = 299792.458;
-    int m = 3; // Number of parameters per component.
+    int m = 3; // Number of parameters per gaussian.
     int n;
     int n_functions = p_size / m; // Number of gaussians.
 
-    for (int i = 0; i < wl_size; i++){
-        flux[i] = 0.0;
-    }
+    memset(flux, 0.0, wl_size * sizeof(double));
 
     for (int k = 0; k < p_size; k += m) {
         n = k / m;
         for (int i = 0; i < wl_size; i++) {
-            lam_ratio = pow(wavelength[i] / rest_wavelength[n], 2.0);
+            // lam_ratio is the square of lambda_received divided by lambda_source.
+            lam_ratio = (wavelength[i] * wavelength[i]) / (rest_wavelength[n] * rest_wavelength[n]);
+            // vel is the velocity
             vel = c * (lam_ratio - 1.0) / (lam_ratio + 1.0);
-            w = - pow((vel - parameters[k + 1]) / parameters[k + 2], 2.0) / 2.0;
-            flux[i] += parameters[k] * exp(w) / (1.0 + (vel / c));
+            // w is the square of ((velocity - velocity_0) / sigma)
+            w = ((vel - parameters[k + 1]) / parameters[k + 2]) * ((vel - parameters[k + 1]) / parameters[k + 2]);
+            flux[i] += parameters[k] * exp(- w / 2.0) / (1.0 + (vel / c));
         }
     }
 
@@ -92,10 +68,11 @@ static PyObject *gauss_vel(PyObject *self, PyObject *args) {
 
     // Return the NumPy array (ownership transferred to the caller)
     return flux_array;
-}
+};
 
 static PyMethodDef methods[] = {
-    {"gauss_vel", gauss_vel, METH_VARARGS, "Gaussian in velocity space"},
+    {"gauss_vel", gauss_vel, METH_VARARGS, "Gaussians in velocity space."},
+    // {"gausshermite_vel", gauss_vel, METH_VARARGS, "Gaussians in velocity space."},
     {NULL, NULL, 0, NULL}
 };
 
@@ -110,4 +87,4 @@ static struct PyModuleDef moduledef = {
 PyMODINIT_FUNC PyInit_elprofile(void) {
     import_array();  // Initialize NumPy API
     return PyModule_Create(&moduledef);
-}
+};
