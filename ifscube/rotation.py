6765ab1b58ef02b20b2a14a0a27b2a692a80cc33
Daniel <daniel.ruschel@ufsc.br>
2025-06-28 17:07:35 -0300

    Introduced possibility of interactive fit
    The main goal of this commit was the implementation of an interactive tool to make the initial guess for the fit paramaters. However, lots of little things appeared along the way. For instance, the parameters that are given in degrees in the configuration file are now converted to radians according to a predefined list also given in the configuration file. The conversion is done only when applying the values to the model, and anywhere else.

--- ifscube/rotation.py
+++ ifscube/rotation.py
@@ -1,13 +1,14 @@
 import argparse
+import json
+import warnings
 from configparser import ConfigParser
 
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 from astropy.modeling.fitting import LevMarLSQFitter
-from matplotlib.widgets import Slider, TextBox, Button
+from matplotlib.widgets import Slider, TextBox, Button, RadioButtons
 from numpy import ma
-import json
 
 from ifscube.models import DiskRotation
 
@@ -15,7 +16,7 @@ from ifscube.models import DiskRotation
 class Rotation(object):
     """Class for fitting rotation models to velocity fields."""
 
-    def __init__(self, input_data=None, extension=0, plane=0, model=None):
+    def __init__(self, input_data=None, extension=0, plane=0, model=None, parameters_in_degrees: list = None):
         """
         Initializes the instance and optionally loads the observation data.
 
@@ -62,10 +63,15 @@ class Rotation(object):
         else:
             self.model = model
 
+        if parameters_in_degrees is None:
+            self.parameters_in_degrees = []
+        else:
+            self.parameters_in_degrees = parameters_in_degrees
+
         self.solution = None
         self.best_fit = np.array([])
 
-    def update_model(self, parameters):
+    def update_model(self, parameters: dict):
         """
         Updates the model parameters.
 
@@ -74,13 +80,18 @@ class Rotation(object):
         parameters: configparser.Configparser
             Dictionary of parameters.
         """
-
         for key in parameters:
-            setattr(self.model, key, parameters[key])
+            if key in self.parameters_in_degrees:
+                setattr(self.model, key, np.deg2rad(parameters[key]))
+            else:
+                setattr(self.model, key, parameters[key])
 
     def update_bounds(self, bounds):
         for key in bounds:
-            self.model.bounds[key] = bounds[key]
+            if key in self.parameters_in_degrees:
+                self.model.bounds[key] = tuple(np.deg2rad(_) for _ in bounds[key])
+            else:
+                self.model.bounds[key] = bounds[key]
 
     def update_fixed(self, fixed):
         for key in fixed:
@@ -98,71 +109,63 @@ class Rotation(object):
         for key in self.solution.param_names:
             n = self.solution.param_names.index(key)
             value = self.solution.parameters[n]
-            if key in ['phi_0', 'theta']:
+            if key in self.parameters_in_degrees:
                 value = np.rad2deg(value)
             color = '\033[91m' if self.solution.parameters[n] in self.model.bounds[key] else ''
             print(f'{color}{key:12s}: {value:8.3f}\033[0m')
 
-    def plot_results(self, contrast=1.0, contours=True, symmetric=True):
+    def plot_results(self, contrast=1.0, contours=True):
         """Plots the fit results."""
 
-        vmin = np.percentile(self.obs[~self.obs.mask], contrast)
-        vmax = np.percentile(self.obs[~self.obs.mask], 100.0 - contrast)
-
-        if symmetric:
-            m = np.max(np.abs([vmin, vmax]))
-            vmin = -m
-            vmax = m
-
+        v_min, v_max = self.get_v_limits(self.obs, contrast=contrast)
+        imshow_opts = dict(cmap='Spectral_r', vmin=v_min, vmax=v_max)
         fig, axes = plt.subplots(nrows=1, ncols=3, sharey='row')
         data = [self.obs, self.best_fit, self.obs - self.best_fit]
         labels = ['Observation', 'Model', 'Residual']
-
-        kwargs = dict(cmap='Spectral_r', vmin=vmin, vmax=vmax)
 
         im = None
         for ax, d, lab in zip(axes, data, labels):
             ax.set_aspect('equal')
             if contours:
-                im = ax.contourf(d, **kwargs)
+                im = ax.contourf(d, **imshow_opts)
             else:
-                im = ax.imshow(d, origin='lower', **kwargs)
+                im = ax.imshow(d, origin='lower', **imshow_opts)
             ax.set_title(lab)
 
         fig.colorbar(im, ax=axes[1], orientation='horizontal')
         plt.show()
 
-    def interactive_guess(self, contrast=1.0):
-        v_min = np.percentile(self.obs[~self.obs.mask], contrast)
-        v_max = np.percentile(self.obs[~self.obs.mask], 100.0 - contrast)
-        kwargs = dict(cmap='Spectral_r', vmin=v_min, vmax=v_max)
-
+    @staticmethod
+    def get_v_limits(data, contrast: float = 1.0):
+        v_min = np.percentile(data.data[~data.mask], contrast)
+        v_max = np.percentile(data.data[~data.mask], 100.0 - contrast)
         m = np.max(np.abs([v_min, v_max]))
         v_min = -m
         v_max = m
+        return v_min, v_max
 
+    @staticmethod
+    def highlight_active(sliders, fig, highlighted):
+        for key in sliders:
+            color = 'lightblue' if key == highlighted else 'lightgray'
+            sliders[key].ax.set_facecolor(color)
+        fig.canvas.draw_idle()
+
+    def interactive_guess(self, params_init: dict, contrast=1.0):
         velocity_map = self.obs
-
-        # -- Initial parameter values --
-        params_init = {
-            'amplitude': 200.0,
-            'x_0': 0.0,
-            'y_0': 0.0,
-            'phi_0': 0.0,
-            'c_0': 1.0,
-            'p': 1.2,
-            'theta': 45.0,
-        }
+        v_min, v_max = self.get_v_limits(data=velocity_map, contrast=contrast)
+        kwargs = dict(cmap='Spectral_r', vmin=v_min, vmax=v_max)
 
         # -- Parameter ranges for sliders --
         param_ranges = {
+            'v_sys': (-300.0, 300.0),
             'amplitude': (100, 500),
-            'x_0': (-300, 300),
-            'y_0': (-300, 300),
-            'phi_0': (0, 180),
+            'x_0': (self.x.min(), self.x.max()),
+            'y_0': (self.y.min(), self.y.max()),
+            'phi_0': (-90.0, 90.0),
             'c_0': (1.0, 10.0),
             'p': (1.0, 1.5),
-            'theta': (0.0, 90.0),
+            'theta': (-90.0, 90.0),
         }
 
         # -- Create figure layout --
@@ -174,18 +177,12 @@ class Rotation(object):
         ax_resid = fig.add_subplot(gs[0:2, 2])
 
         # -- Display observed image --
-        im_obs = ax_obs.imshow(velocity_map, origin='lower', **kwargs)
+        ax_obs.imshow(velocity_map, origin='lower', **kwargs)
         ax_obs.set_title("Observed")
 
-        # -- Call external model --
-        def get_model(params):
-            for key in params:
-                if key in ['phi_0', 'theta']:
-                    params[key] = np.deg2rad(params[key])
-
-            return DiskRotation(**params)(self.x, self.y)
-
-        model = get_model(params_init)
+        # The initial parameters are already in radians
+        self.update_model(params_init)
+        model = self.model(self.x, self.y)
         im_model = ax_model.imshow(model, origin='lower', **kwargs)
         ax_model.set_title("Model")
 
@@ -199,18 +196,21 @@ class Rotation(object):
 
         slider_height = 0.025
         spacing = 0.035
-        bottom_start = 0.28
+        bottom_start = 0.35
+
+        print("Parameters can be adjusted using the sliders, the text boxes, or the left and right arrows on"
+              " your keyboard. Use the radio buttons to select which parameter should respond to the arrow keys.")
 
         for i, (param, val) in enumerate(params_init.items()):
             y_pos = bottom_start - i * spacing
 
             # Slider (left 70% of the width)
-            ax_slider = plt.axes([0.15, y_pos, 0.6, slider_height])
+            ax_slider = plt.axes((0.35, y_pos, 0.4, slider_height))
             slider = Slider(ax_slider, param, *param_ranges[param], valinit=val)
             sliders[param] = slider
 
             # TextBox (right 10%)
-            ax_box = plt.axes([0.85, y_pos, 0.08, slider_height])
+            ax_box = plt.axes((0.85, y_pos, 0.08, slider_height))
             textbox = TextBox(ax_box, '', initial=str(val))
             textboxes[param] = textbox
 
@@ -226,29 +226,32 @@ class Rotation(object):
                     try:
                         num = float(text)
                         min_val, max_val = param_ranges[p]
-                        if min_val <= num <= max_val:
-                            sliders[p].set_val(num)
-                        else:
+                        if (num < min_val) or (num > max_val):
                             print(f"{p}: Value out of range")
+                        sliders[p].set_val(num)
                     except ValueError:
                         print(f"{p}: Invalid input")
 
                 return submit
 
             textbox.on_submit(make_textbox_submit(param))
+
         # -- Update on slider change --
         def update(val):
             current = {k: s.val for k, s in sliders.items()}
-            new_model = get_model(current)
+            self.update_model(current)
+            new_model = self.model(self.x, self.y)
             im_model.set_data(new_model)
             im_resid.set_data(velocity_map - new_model)
+            rms = np.sqrt(np.sum(np.square(velocity_map - new_model)) / velocity_map.size)
+            ax_resid.set_title(f"RMS = {rms:.3f}")
             fig.canvas.draw_idle()
 
         for slider in sliders.values():
             slider.on_changed(update)
 
         # Save button
-        ax_button = plt.axes([0.40, 0.01, 0.2, 0.04])
+        ax_button = plt.axes((0.40, 0.01, 0.2, 0.04))
         save_button = Button(ax_button, 'Save')
 
         def save_params(event):
@@ -259,7 +262,27 @@ class Rotation(object):
 
         save_button.on_clicked(save_params)
 
-        plt.tight_layout()
+        parameter_names = list(param_ranges.keys())
+        rax = plt.axes((0.1, bottom_start - len(parameter_names) * spacing, 0.1, bottom_start + spacing))
+        radio = RadioButtons(rax, labels=parameter_names)
+
+        active_slider_key = [parameter_names[0]]
+
+        def on_radio(label):
+            active_slider_key[0] = label
+
+        radio.on_clicked(on_radio)
+
+        def on_key(event):
+            key = active_slider_key[0]
+            step = (param_ranges[key][1] - param_ranges[key][0]) / 100.0
+            if event.key == 'right':
+                sliders[key].set_val(sliders[key].val + step)
+            elif event.key == 'left':
+                sliders[key].set_val(sliders[key].val - step)
+
+        fig.canvas.mpl_connect('key_press_event', on_key)
+
         plt.show()
 
 
@@ -267,7 +290,9 @@ class Config(ConfigParser):
 
     def __init__(self, file_name):
         ConfigParser.__init__(self)
+        print(f"Reading configuration file '{file_name}' ...", end=" ")
         self.read(file_name)
+        print("Done!")
 
         if 'general' in self.sections():
             self._read_general()
@@ -298,24 +323,25 @@ class Config(ConfigParser):
         self.general = {'fit': self.getboolean('general', 'fit')}
 
     def _read_loading(self):
-        self.loading = {'input_data': self.get('loading', 'input_data')}
+        self.loading = {}
+        if "input_data" in self.options("loading"):
+            self.loading['input_data'] = self.get('loading', 'input_data')
+        else:
+            warnings.warn("No input data read from configuration file!")
         if 'extension' in self['loading']:
             e = self.get('loading', 'extension')
             self.loading['extension'] = (int(e) if e.isdigit() else e)
         self.loading['plane'] = self.getint('loading', 'plane')
+        self.loading['parameters_in_degrees'] = [_.strip() for _ in
+                                                 self.get('loading', 'parameters_in_degrees').split(',')]
 
     def _read_model(self):
         self.model = {key: self.getfloat('model', key) for key in self['model']}
-        for key in ['phi_0', 'theta']:
-            if key in self.model:
-                self.model[key] = np.deg2rad(self.model[key])
 
     def _read_bounds(self):
         self.bounds = {}
         for key in self['bounds']:
             b = tuple([float(i) for i in self['bounds'][key].split(',')])
-            if key in ['phi_0', 'theta']:
-                b = tuple([np.deg2rad(i) for i in b])
             self.bounds.update({key: b})
 
     def _read_fixed(self):
@@ -342,18 +368,15 @@ def main():
     r.update_model(config.model)
 
     if args.interactive:
-        r.interactive_guess()
+        r.interactive_guess(params_init=config.model)
 
-    # if config.getboolean('general', 'fit'):
-    #     r.update_bounds(config.bounds)
-    #     r.update_fixed(config.fixed)
-    #     r.fit_model(maxiter=1000)
-    # else:
-    #     r.solution = r.model
-    #     r.best_fit = r.model(r.x, r.y)
+    if config.getboolean('general', 'fit'):
+        r.update_bounds(config.bounds)
+        r.update_fixed(config.fixed)
+        r.fit_model(maxiter=1000)
+    else:
+        r.solution = r.model
+        r.best_fit = r.model(r.x, r.y)
 
-    # r.print_solution()
-    # r.plot_results(contours=False)
-
-if __name__ == '__main__':
-    main()
+    r.print_solution()
+    r.plot_results(contours=False)
