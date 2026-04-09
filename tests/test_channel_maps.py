c272c150863be2736eb8b307f3267573e1dd0997
Daniel <daniel.ruschel@ufsc.br>
2026-04-09 15:39:20 -0300

    Fixed path to example file.

--- tests/test_channel_maps.py
+++ tests/test_channel_maps.py
@@ -6,7 +6,7 @@ from src.ifscube import Cube
 
 
 def test_deprecation_warning():
-    cube_file = str(importlib.resources.files("ifscube") / "examples/ngc3081_cube.fits")
+    cube_file = str(importlib.resources.files("ifscube") / "../../examples/ngc3081_cube.fits")
     c = Cube(fname=cube_file)
     with pytest.warns(DeprecationWarning):
         channel_maps.channelmaps(cube=c, lambda0=6000.0, vel_min=-100.0, vel_max=100.0, screen=False)
